bf9a16eee21f38764a182f8fece7e819ff412b69
Tyler Hancock <darklime@live.ca>
2020-10-04 23:56:53 -0600

    Initial version.

---
+++ src/main/java/net/darkhax/botanytrees/BotanyTrees.java
@@ -0,0 +1,13 @@
+package net.darkhax.botanytrees;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BotanyTrees.MOD_ID)
+public class BotanyTrees {
+    
+    public static final String MOD_ID = "botanytrees";
+    public static final Logger LOGGER = LogManager.getLogger("Botany Trees");
+}
