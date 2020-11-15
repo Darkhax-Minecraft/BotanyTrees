305f6d49a01855561464d697752fa8621b4800e6
Tyler Hancock <darklime@live.ca>
2020-11-15 05:23:07 -0700

    Removed unused code.

--- src/main/java/net/darkhax/botanytrees/BotanyTrees.java
+++ src/main/java/net/darkhax/botanytrees/BotanyTrees.java
@@ -1,13 +1,7 @@
 package net.darkhax.botanytrees;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(BotanyTrees.MOD_ID)
+@Mod("botanytrees")
 public class BotanyTrees {
-    
-    public static final String MOD_ID = "botanytrees";
-    public static final Logger LOGGER = LogManager.getLogger("Botany Trees");
 }
